7d9b80b1646c154303b90135022f66bb5b66149f
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-02-07 14:16:57 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const baseUrl = "https://fakestoreapi.com/products";
+  const instance = axios.create({
+    baseURL: "https://fakestoreapi.com/products",
+    timeout: 1000,
+    headers: { "Secret-Custom-Header": "token" },
+  });
+
+  useEffect(() => {
+    instance
+      .get()
+      .then(function (response) {
+        setProducts(response.data);
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  if (products.length < 1) return <p>Loading...</p>;
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>{products[0].title}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
